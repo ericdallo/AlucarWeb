0b32189a6f1baffb727ba3e790d5b94cceb59bd9
Eric Dallo <ericdallo06@hotmail.com>
2015-10-10 13:03:18 -0300

    Ao final de cada request, é fechado o manager

--- src/main/java/com/alucarweb/util/EntityManagerProducer.java
+++ src/main/java/com/alucarweb/util/EntityManagerProducer.java
@@ -1,24 +1,29 @@
 package com.alucarweb.util;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-@RequestScoped
+@ApplicationScoped
 public class EntityManagerProducer {
 
+	@Inject
 	private EntityManager entityManager;
 
 	private static EntityManagerFactory factory = Persistence.createEntityManagerFactory("alucar");
 
 	@Produces
+	@RequestScoped
 	public EntityManager getEntityManager() {
+		return entityManager = factory.createEntityManager();
+	}
 
-		if (entityManager == null) {
-			entityManager = factory.createEntityManager();
-		}
-		return entityManager;
+	public void closeEntityManager(@Disposes EntityManager manager) {
+		manager.close();
 	}
 }
