088410539ee9dc4011837a3dc0e9c9f2a40a2f62
Eric Dallo <ericdallo06@hotmail.com>
2015-10-12 17:49:53 -0300

    Loga quando nao consegue pegar a imagem

--- src/main/java/com/alucarweb/car/images/ImagesService.java
+++ src/main/java/com/alucarweb/car/images/ImagesService.java
@@ -6,23 +6,27 @@ import java.io.IOException;
 import javax.inject.Inject;
 import javax.servlet.ServletContext;
 
+import org.apache.commons.logging.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.alucarweb.aws.AlucarS3;
 import com.alucarweb.car.Car;
 
 import br.com.caelum.vraptor.observer.upload.UploadedFile;
 
-public class Images {
+public class ImagesService {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(ImagesService.class);
 	private static final String SUFIX = "-";
 
 	@Inject
 	private ServletContext context;
+
 	@Inject
 	private AlucarS3 alucarS3;
 
 	public void save(Car car, UploadedFile uploadedFile) {
-
-		// String path = "/home/kali/github/AlucarWeb/";
 
 		File imageFile = new File(context.getRealPath("images"), uploadedFile.getFileName());
 
@@ -32,7 +36,7 @@ public class Images {
 			uploadedFile.writeTo(imageFile);
 			alucarS3.send(imageFile, car.getId());
 		} catch (IOException e) {
-			e.printStackTrace();
+			LOGGER.error("Failed to convert UploadedImage to File witg car id {}.", car.getId(), e);
 		}
 	}
 }
