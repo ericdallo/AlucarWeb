f344bf5ebb1fb7b53efe9a13f865e40292017077
Eric Dallo <ericdallo06@hotmail.com>
2015-10-15 22:06:53 -0300

    Corrige deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@ cd /home/ec2-user/AlucarDeploy
 
 mvn clean compile package
 
-sudo cp -rfv target/*.war /usr/share/tomcat7/webapps/Alucar.war
+cp -rfv target/*.war /usr/share/tomcat7/webapps/Alucar.war
 
-sudo service tomcat7 restart
+service tomcat7 restart
 
