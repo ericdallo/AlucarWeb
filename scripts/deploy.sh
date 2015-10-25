2919cc03f84554d6cf3b450e399254cd949359b3
Eric Dallo <ericdallo06@hotmail.com>
2015-10-25 12:39:42 -0200

    Corrige script de deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,12 @@ cd /home/ec2-user/AlucarDeploy
 
 mvn package
 
-cp -rfv target/*.war /usr/share/tomcat7/webapps/AlucarWeb.war
+WAR_PATH=$(ls target/*.war |awk '{print $1}' |tail -1)
 
-service tomcat7 restart
+rm -rfv /usr/share/tomcat7/webapps/AlucarWeb
+sudo cp -rfv $WAR_PATH /usr/share/tomcat7/webapps/AlucarWeb.war
+
+sudo cp -rfv ../alucar.properties /usr/share/tomcat7/webapps/AlucarWeb/WEB-INF/classes/com/alucarweb/util/alucar.properties
+
+sudo service tomcat7 restart
 
