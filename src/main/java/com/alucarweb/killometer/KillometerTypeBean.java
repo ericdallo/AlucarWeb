367aa685c875d3c97c900c1631e6b28e27dc1403
Ramon Andrade <andrade.rmn@gmail.com>
2015-10-27 18:50:32 -0200

    WIP

--- src/main/java/com/alucarweb/killometer/KillometerTypeBean.java
+++ src/main/java/com/alucarweb/killometer/KillometerTypeBean.java
@@ -4,9 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class KillometerTypeBean {
-	
+
 	public List<KillometerType> getList() {
 		return Arrays.asList(KillometerType.values());
-	}
-	
+	}	
 }
